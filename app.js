2f00e523bd0960187e97996596a899bc0b567199
Sri Sai Poosarla <sairampoosarlahyd@gmail.com>
2025-05-04 19:27:53 +1000

    database connection has been added; basic get route has been added; middleware like express.json, 404 for found, error handler have been added

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log('E-Commerce API');
+const express = require('express')
+require('dotenv').config()
+require('express-async-errors')
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+//importing db file
+const connectDB = require('./db/connect')
+
+const app = express()
+
+app.use(express.json())
+
+//404 not found middleware
+app.use(notFound)
+
+//error handeling middleware
+app.use(errorHandlerMiddleware)
+
+app.get('/', (req, res) => {
+    res.send('e-commerse api')
+})
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try{
+        console.log(process.env.MONGO_URI)
+        await connectDB(process.env.MONGO_URI);
+
+        app.listen(port, () =>{
+            console.log(` listening on port ${port}...`)
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+start()
