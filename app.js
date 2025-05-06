3c5a9561603fcc5c8ad2cf07c11d348475ab2287
Sri Sai Poosarla <sairampoosarlahyd@gmail.com>
2025-05-06 22:32:39 +1000

    added the user model schema and it contains a validator where we can pass a validator which checks for valid email

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ require('dotenv').config()
 require('express-async-errors')
 const notFound = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
+const morgan = require('morgan')
 
 //importing db file
 const connectDB = require('./db/connect')
 
 const app = express()
 
+app.use(morgan('tiny'))
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('e-commerse api')
+})
 
 //404 not found middleware
 app.use(notFound)
 
 //error handeling middleware
 app.use(errorHandlerMiddleware)
-
-app.get('/', (req, res) => {
-    res.send('e-commerse api')
-})
 
 const port = process.env.PORT || 5000
 
---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, 'Please Provide name of the user'],
+        minlength: 3,
+        maxlength: 20
+    },
+    email:{
+        type: String,
+        required: [true, "Please provide email"],
+        //here in the validator we are passed a function which check if the email is valid
+        validate:{
+            //validator package has the function which provides if it is a valid email
+            validator: validator.isEmail,
+            message: "please provide valid email"
+        }
+    },
+    password:{
+        type: String,
+        required: [true, 'please provide passsword'],
+        minlength:6,
+    },
+    role:{
+        type:String,
+        enum:['user','admin'],
+        default:'user'
+    }
+})
+
+module.exports = mongoose.model("User", UserSchema)
